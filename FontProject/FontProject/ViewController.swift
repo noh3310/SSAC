ac3a17839a9f5dc16051ecb92fec9832ef60adb4
GunHo <noh3310@gmail.com>
2021-11-01 18:06:09 +0900

    블로그 관련 자료

--- FontProject/FontProject/ViewController.swift
+++ FontProject/FontProject/ViewController.swift
@@ -2,18 +2,24 @@
 //  ViewController.swift
 //  FontProject
 //
-//  Created by 노건호 on 2021/11/01.
+//  Created by Jerry on 2021/11/01.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sfFontLabel: UILabel!
+    @IBOutlet weak var bmFontLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sfFontLabel.text = "샌프란시스코 폰트"
+        sfFontLabel.font = UIFont(name: "SFPro-Regular", size: 20)
+
+        bmFontLabel.text = "우아한형제들 폰트"
+        bmFontLabel.font = UIFont(name: "BMJUAOTF", size: 20)
     }
-
-
 }
 
