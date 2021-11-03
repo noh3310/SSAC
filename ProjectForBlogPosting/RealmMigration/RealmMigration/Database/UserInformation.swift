72fa623e5bebaa1e18dd6778f9120d39cd789d3a
GunHo <noh3310@gmail.com>
2021-11-04 02:31:10 +0900

    Blog Project

--- ProjectForBlogPosting/RealmMigration/RealmMigration/Database/UserInformation.swift
+++ ProjectForBlogPosting/RealmMigration/RealmMigration/Database/UserInformation.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import RealmSwift
+
+class UserInformation: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    
+    @Persisted var identifier: String
+    @Persisted var password: String
+    @Persisted var name: String
+    @Persisted var birthday: Date
+    
+    convenience init(identifier: String, password: String, name: String, birthday: Date) {
+        self.init()
+        
+        self.identifier = identifier
+        self.password = password
+        self.name = name
+        self.birthday = birthday
+    }
+}
--- ProjectForBlogPosting/RealmMigration/RealmMigration/ViewController.swift
+++ ProjectForBlogPosting/RealmMigration/RealmMigration/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
-
+    
+    var userList: Results<UserInformation>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1. config 설정(이전 버전에서 다음 버전으로 마이그레이션될때 어떻게 변경될것인지)
+        let config = Realm.Configuration(
+            schemaVersion: 2, // 새로운 스키마 버전 설정
+            migrationBlock: { migration, oldSchemaVersion in
+                if oldSchemaVersion < 2 {
+                    // 1-1. 마이그레이션 수행(버전 2보다 작은 경우 버전 2에 맞게 데이터베이스 수정)
+                    migration.enumerateObjects(ofType: UserInformation.className()) { oldObject, newObject in
+                        newObject!["birthday"] = Date()
+                    }
+                }
+            }
+        )
+        
+        // 2. Realm이 새로운 Object를 쓸 수 있도록 설정
+        Realm.Configuration.defaultConfiguration = config
+        
+        // 3. Realm에게 스키마 처리방법을 알려줬기 때문에 자동으로 Migration을 수행
+        let realm = try! Realm()
+        
+        userList = realm.objects(UserInformation.self)
     }
-
-
 }
 
