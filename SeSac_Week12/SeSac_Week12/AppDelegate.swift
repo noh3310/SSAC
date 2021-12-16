8fb1717ead37285d893471dd85610d701a91bb7d
GunHo <noh3310@gmail.com>
2021-12-16 12:25:07 +0900

    2021-12-16

--- SeSac_Week12/SeSac_Week12/AppDelegate.swift
+++ SeSac_Week12/SeSac_Week12/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print(#function)
         // Override point for customization after application launch.
+        
+        // 공유 인스턴스로 초기화
+        FirebaseApp.configure()
+        
+        // 권한에 대한 부분
+        if #available(iOS 10.0, *) {
+          // For iOS 10 display notification (sent via APNS)
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+          )
+        } else {
+          let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        // 이 앱이
+        application.registerForRemoteNotifications()
+        
+        // 메시지 대리자 설정
+        // APNS, 클라이언트의 역할을 위임하는것
+        Messaging.messaging().delegate = self
+        
+        Messaging.messaging().token { token, error in
+          if let error = error {
+              print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+              print("FCM registration token: \(token)")
+//            self.fcmRegTokenMessage.text  = "Remote FCM registration token: \(token)"
+              print(token)
+          }
+        }
+        
         return true
     }
 
@@ -37,3 +76,58 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // 기기의 토큰과 파이어베이스에서 오는 토큰을 일치시켜준다고 보면 된다.
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // 포그라인드 수신: willPresent
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // iOS 14 이상: banner, list
+        if #available(iOS 14.0, *) {
+            completionHandler([.list, .banner, .badge, .sound])
+        } else {
+            completionHandler([.alert, .badge, .sound])
+        }
+    }
+    
+    // 사용자가 로컬/푸시를 클릭했을 떄 Response 호출 메서드
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        print("사용자가 푸시를 클릭했습니다.")
+        
+        // userInfo: key - 1(광고), 2(채팅방), 3(사용자 설정)
+        // 각 데이터마다 화면을 다르게 설정, 알람을 클릭했을 떄 그 알람으로 가야한다.
+        print(response.notification.request.content.userInfo)
+        print(response.notification.request.content.body)
+        
+        let userInfo = response.notification.request.content.userInfo
+        if userInfo[AnyHashable("key")] as? Int == 1 {
+            print("광고 푸시입니다.")
+        } else {
+            print("다른 푸시입니다.")
+        }
+        
+        completionHandler()
+    }
+    
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+
+}
